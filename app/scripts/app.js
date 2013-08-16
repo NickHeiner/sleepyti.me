2fdf5287fe69a8afdb517a496e70a0ef06a0b87f
Nick Heiner <nick.heiner@opower.com>
2013-08-15 23:09:55 -0400

    initial import from yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('sleepyti.meApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
