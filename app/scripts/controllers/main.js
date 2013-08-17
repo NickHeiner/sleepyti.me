ba316be16dfeccef436b273bda3042c4de4fd68f
Nick Heiner <nick.heiner@opower.com>
2013-08-16 22:36:11 -0400

    hooking service into ui

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,22 @@
 'use strict';
 
 angular.module('sleepyti.meApp')
-    .controller('MainCtrl', function($scope) {
+    .controller('MainCtrl', function($scope, offsetFromWakeup, offsetFromSleep, createMoment) {
         $scope.hours = _.range(1, 13);
         $scope.minutes = _.range(1, 61);
         $scope.amPm = ['AM', 'PM'];
 
-        $scope.wakeup = {
+        $scope.sleep = $scope.wakeup = {
             hour: 1,
             minute: 1,
             am: 'AM'
         };
+
+        $scope.$watch('[wakeup.hour, wakeup.minute]', function() {
+            $scope.sleepTimes = offsetFromWakeup(createMoment($scope.wakeup.hour, $scope.wakeup.minute));
+        }, true);
+
+        $scope.$watch('[sleep.hour, sleep.minute]', function() {
+            $scope.wakeupTimes = offsetFromSleep(createMoment($scope.sleep.hour, $scope.sleep.minute));
+        });
     });
---
+++ app/scripts/services/create-moment.js
@@ -0,0 +1,9 @@
+angular.module('sleepyti.meApp')
+    .factory('createMoment', function() {
+        return function(hour, minute) {
+            var result = moment();
+            result.hours(hour);
+            result.minutes(minute);
+            return result;
+        }
+    });
