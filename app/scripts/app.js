c3ddb024e59e3ea37e40384403f1eac19d43d1cc
Nick Heiner <nick.heiner@opower.com>
2013-08-18 12:29:55 -0400

    cleaning out routing cruft;

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,3 @@
 'use strict';
 
-angular.module('sleepyti.meApp', [])
-    // TODO clean this out
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('sleepyti.meApp', []);
